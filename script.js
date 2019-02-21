353ae0023f37131734d0e35ae37b8b83427ce30c
Victor Manuel Herrera Reverter <victormanuel.cuba@gmail.com>
2019-02-21 06:24:26 +0100

    Second Update Champions Draw

--- script.js
+++ script.js
@@ -30,13 +30,48 @@ const request = async () => {
     try {
         const response = await fetch('teams.json');
         const json = await response.json();
-        console.log(json);
-
-
+        // console.log(json);
+        return json;        // this return the all data and allow us use it in following promises
     } catch (err) {
         console.log('%c Ohh this is the error => ', 'color: red; font-size:15px', err);
         
     }
 }
 
-request();
+// Extract the data from the async/await
+
+request()
+    .then(
+        (json) => {     // bring here all the JSON data
+            // console.log(json);
+            var {bombo1, bombo2, bombo3, bombo4} = json;
+            // console.log(bombo1);
+            // console.log(bombo2);
+            // console.log(bombo3);
+            // console.log(bombo4);
+
+            var getTeams = (bomboValues) => {
+                var bomboKeys = Object.keys(bomboValues);
+                for (const keysObj in bomboKeys) {
+                    // console.log(keysObj); 
+                    // console.log(bomboKeys[keysObj]);
+                    var a = bomboKeys[keysObj];
+                    console.log(bomboValues[a]); // access to Teams Objects   
+                }
+            }
+
+            // access to all the bombos teams
+
+            for (let bomboVal = 1; bomboVal <= 4; bomboVal++) {
+                // console.log(bomboVal);
+                var currentBombo = 'bombo' + bomboVal;
+                console.log(currentBombo);
+                getTeams(eval(currentBombo));
+            }
+
+            // getTeams(bombo1);
+            
+                    
+        }    
+    )
+
